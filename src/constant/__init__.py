1e96624f6a845dca908595408b8bfabd3b54d2c3
AdityaTheJaat <programmingID1306@gamil.com>
2024-07-30 09:42:59 +0530

    contants updated

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import os
 
-AWS_S3_BUCKET_NAME = "sensor-deploymentpw1"
-MONGO_DATABASE_NAME = "Phishing"
+AWS_S3_BUCKET_NAME = "ML Projects"
+MONGO_DATABASE_NAME = "phishing"
 
 TARGET_COLUMN = "Result"
 
